7f6e20f8133569f6ff7cfbf308c82d929e68be83
Roy Anger <roy@royanger.com>
2024-04-18 16:29:50 -0400

    Some minor cleanup/changes to the pages and code

---
+++ app/consts/cards.ts
@@ -0,0 +1,22 @@
+export const CARDS = [
+  {
+    title: "Customizable Components",
+    description:
+      "Prebuilt components to handle essential functionality like user sign-in, sign-up, and account management.",
+    href: "https://clerk.com/docs/components/overview?utm_source=vercel-template&utm_medium=template_repos&utm_campaign=nextjs_template",
+    linkText: "Components",
+  },
+  {
+    title: "React Hooks",
+    description: `Build custom functionality by accessing auth state, user and session data, and more with Clerk's React Hooks.`,
+    href: "https://clerk.com/docs/references/react/use-user?utm_source=vercel-template&utm_medium=template_repos&utm_campaign=nextjs_template",
+    linkText: "React Hooks",
+  },
+  {
+    title: "Multitenancy",
+    description:
+      "Seamlessly create and switch between organizations, invite and manage members, and assign custom roles.",
+    href: "https://clerk.com/docs/organizations/overview?utm_source=vercel-template&utm_medium=template_repos&utm_campaign=nextjs_template",
+    linkText: "Components",
+  },
+];
