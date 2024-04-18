457f0d7aca1a905e0a3a92f6117d810f4d395825
Roy Anger <roy@royanger.com>
2024-04-18 17:04:42 -0400

    Re-added @ts-expect-error -- missed that trying to cherry pick actual changes vs formatting changes

--- app/components/code-switcher.tsx
+++ app/components/code-switcher.tsx
@@ -48,6 +48,7 @@ export function CodeSwitcher() {
       </div>
       <div className="relative" style={{ height: "calc(100% - 42px)" }}>
         <div className="mask h-full">
+          {/* @ts-expect-error */}
           <SyntaxHighlighter language="javascript" style={theme}>
             {selectedCode}
           </SyntaxHighlighter>
