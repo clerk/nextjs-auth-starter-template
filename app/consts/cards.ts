c813e959eebfba0d348165edbb5031df161fd549
Braden Sidoti <braden@clerk.dev>
2024-04-30 15:35:26 -0600

    card content changes

--- app/consts/cards.ts
+++ app/consts/cards.ts
@@ -3,21 +3,21 @@ export const CARDS = [
     title: "Customizable Components",
     description:
       "Prebuilt components to handle essential functionality like user sign-in, sign-up, and account management.",
-    href: "https://clerk.com/docs/components/overview?utm_source=vercel-template&utm_medium=template_repos&utm_campaign=nextjs_template",
-    linkText: "Components",
+    href: "https://clerk.com/docs/components/overview?utm_source=vercel-template&utm_medium=partner&utm_term=component_reference",
+    linkText: "Component Reference",
   },
   {
     title: "React Hooks",
     description: `Build custom functionality by accessing auth state, user and session data, and more with Clerk's React Hooks.`,
-    href: "https://clerk.com/docs/references/react/use-user?utm_source=vercel-template&utm_medium=template_repos&utm_campaign=nextjs_template",
+    href: "https://clerk.com/docs/references/react/use-user?utm_source=vercel-template&utm_medium=partner&utm_term=react_hooks",
     linkText: "React Hooks",
   },
   {
-    title: "Multitenancy",
+    title: "Organizations",
     description:
-      "Seamlessly create and switch between organizations, invite and manage members, and assign custom roles.",
-    href: "https://clerk.com/docs/organizations/overview?utm_source=vercel-template&utm_medium=template_repos&utm_campaign=nextjs_template",
-    linkText: "Components",
+      "Built for B2B SaaS: create and switch between orgs, manage and invite members, and assign custom roles.",
+    href: "https://clerk.com/docs/organizations/overview?utm_source=vercel-template&utm_medium=partner&utm_term=component_reference",
+    linkText: "Organizations",
   },
 ];
 
@@ -27,19 +27,19 @@ export const DASHBOARD_CARDS = [
     description:
       "Clerk empowers you to authenticate same and cross origin requests using a Clerk generated JWT",
     href: "https://clerk.com/docs/backend-requests/overview?utm_source=vercel-template&utm_medium=partner&utm_term=JWT",
-    linkText: "JWT",
+    linkText: "Request authentication",
   },
   {
     title: "Build an onboarding flow",
     description: `Leverage customizable session tokens, public metadata, and Middleware to create a custom onboarding experience.`,
-    href: "https://clerk.com/docs/backend-requests/overview?utm_source=vercel-template&utm_medium=partner&utm_term=onboarding",
-    linkText: "Onboarding",
+    href: "https://clerk.com/docs/guides/add-onboarding-flow?utm_source=vercel-template&utm_medium=partner&utm_term=onboarding",
+    linkText: "Onboarding flow",
   },
   {
     title: "Deploy to Production",
     description:
       "Production instances are meant to support high volumes of traffic and by default, have a more strict security posture.",
-    href: "https://clerk.com/docs/backend-requests/overview?utm_source=vercel-template&utm_medium=partner&utm_term=deploy-to-prod",
+    href: "https://clerk.com/docs/deployments/overview?utm_source=vercel-template&utm_medium=partner&utm_term=deploy-to-prod",
     linkText: "Production",
   },
 ];
