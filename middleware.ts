317a664277078b5db13e05c066d5c8fa9a081860
Domitrius <domitriusaclark@gmail.com>
2024-06-03 17:47:16 -0400

    upgraded to next 15 and react 19 & moved route protection to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/"],
