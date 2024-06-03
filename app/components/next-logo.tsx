7e4fec175f309bdf4d6da2f07150a7319bd22d97
Domitrius <domitriusaclark@gmail.com>
2024-06-03 18:13:06 -0400

    moved logo svg's to their own components to clean up the dashboard

---
+++ app/components/next-logo.tsx
@@ -0,0 +1,71 @@
+export function NextLogo() {
+  return (
+    <a href="https://nextjs.org/" target="_blank">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <g clipPath="url(#clip0_29_46694)">
+          <mask
+            id="mask0_29_46694"
+            style={{ maskType: "alpha" }}
+            width="24"
+            height="24"
+            x="0"
+            y="0"
+            maskUnits="userSpaceOnUse"
+          >
+            <path
+              fill="#000"
+              d="M12 24c6.627 0 12-5.373 12-12S18.627 0 12 0 0 5.373 0 12s5.373 12 12 12z"
+            ></path>
+          </mask>
+          <g mask="url(#mask0_29_46694)">
+            <path
+              fill="#000"
+              d="M12 24c6.627 0 12-5.373 12-12S18.627 0 12 0 0 5.373 0 12s5.373 12 12 12z"
+            ></path>
+            <path
+              fill="url(#paint0_linear_29_46694)"
+              d="M19.935 21.003L9.219 7.2H7.2v9.596h1.615V9.251l9.852 12.728c.444-.297.868-.624 1.268-.976z"
+            ></path>
+            <path
+              fill="url(#paint1_linear_29_46694)"
+              d="M16.934 7.2h-1.6v9.6h1.6V7.2z"
+            ></path>
+          </g>
+        </g>
+        <defs>
+          <linearGradient
+            id="paint0_linear_29_46694"
+            x1="14.534"
+            x2="19.267"
+            y1="15.533"
+            y2="21.4"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#fff"></stop>
+            <stop offset="1" stopColor="#fff" stopOpacity="0"></stop>
+          </linearGradient>
+          <linearGradient
+            id="paint1_linear_29_46694"
+            x1="16.134"
+            x2="16.107"
+            y1="7.2"
+            y2="14.25"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#fff"></stop>
+            <stop offset="1" stopColor="#fff" stopOpacity="0"></stop>
+          </linearGradient>
+          <clipPath id="clip0_29_46694">
+            <path fill="#fff" d="M0 0H24V24H0z"></path>
+          </clipPath>
+        </defs>
+      </svg>
+    </a>
+  );
+}
