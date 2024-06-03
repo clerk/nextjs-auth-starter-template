cfd066da99438219437e6b0414ffeac425287566
Domitrius <domitriusaclark@gmail.com>
2024-06-03 15:57:46 -0400

    copied versions from CNA & removed reliance on geist as a package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,54 @@
-import { ClerkProvider } from '@clerk/nextjs';
-import './globals.css';
-import Script from 'next/script';
-import { Metadata } from 'next';
-import { GeistSans } from 'geist/font/sans';
-import { GeistMono } from 'geist/font/mono';
-import { LearnMore } from './components/learn-more';
-import { Footer } from './components/footer';
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import Script from "next/script";
+import { Metadata } from "next";
+import localFont from "next/font/local";
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://clerk-next-app.vercel.app/'),
-  title: 'Next.js Clerk Template',
-  description: 'A simple and powerful Next.js template featuring authentication and user management powered by Clerk.',
-  openGraph: { images: ['/og.png'] },
+  metadataBase: new URL("https://clerk-next-app.vercel.app/"),
+  title: "Next.js Clerk Template",
+  description:
+    "A simple and powerful Next.js template featuring authentication and user management powered by Clerk.",
+  openGraph: { images: ["/og.png"] },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+});
+
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <ClerkProvider
         appearance={{
-          variables: { colorPrimary: '#000000' },
+          variables: { colorPrimary: "#000000" },
           elements: {
-            formButtonPrimary: 'bg-black border border-black border-solid hover:bg-white hover:text-black',
+            formButtonPrimary:
+              "bg-black border border-black border-solid hover:bg-white hover:text-black",
             socialButtonsBlockButton:
-              'bg-white border-gray-200 hover:bg-transparent hover:border-black text-gray-600 hover:text-black',
-            socialButtonsBlockButtonText: 'font-semibold',
+              "bg-white border-gray-200 hover:bg-transparent hover:border-black text-gray-600 hover:text-black",
+            socialButtonsBlockButtonText: "font-semibold",
             formButtonReset:
-              'bg-white border border-solid border-gray-200 hover:bg-transparent hover:border-black text-gray-500 hover:text-black',
-            membersPageInviteButton: 'bg-black border border-black border-solid hover:bg-white hover:text-black',
-            card: 'bg-[#fafafa]',
+              "bg-white border border-solid border-gray-200 hover:bg-transparent hover:border-black text-gray-500 hover:text-black",
+            membersPageInviteButton:
+              "bg-black border border-black border-solid hover:bg-white hover:text-black",
+            card: "bg-[#fafafa]",
           },
         }}
       >
-        <body className={`min-h-screen flex flex-col antialiased`}>{children}</body>
+        <body className={`min-h-screen flex flex-col antialiased`}>
+          {children}
+        </body>
       </ClerkProvider>
 
       <Script src="https://cdn.jsdelivr.net/npm/prismjs@1/components/prism-core.min.js" />
