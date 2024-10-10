4fbb0519fe96097f04e6fb318fd689788c9270ac
Alex Booker <booker@booker.codes>
2024-10-10 15:36:03 +0100

    fix casing

--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,7 @@ export default function Home() {
               <SignedOut>
                 <SignInButton>
                   <button className="px-4 py-2 rounded-full bg-[#131316] text-white text-sm font-semibold">
-                    Sign In
+                    Sign in
                   </button>
                 </SignInButton>
               </SignedOut>
