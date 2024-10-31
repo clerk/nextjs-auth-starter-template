08aebd8e545eeb886c63357369c5c0890aea79e6
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2024-10-31 15:35:18 -0300

    refactor: streamline component structure

--- app/components/user-details.tsx
+++ app/components/user-details.tsx
@@ -10,9 +10,9 @@ type RowProps = {
 
 function Row({ desc, value, label }: RowProps) {
   return (
-    <div className="h-[2.125rem] grid grid-cols-2 items-center relative">
-      <span className="text-xs font-semibold">{desc}</span>
-      <span className="text-xs text-[#7D7D7E] font-mono relative flex items-center gap-2">
+    <div className="grid grid-cols-2 items-center h-8 relative text-xs">
+      <span className="font-semibold">{desc}</span>
+      <span className="text-gray-600 font-mono flex items-center">
         <span className="truncate">{value}</span>
         <Pointer label={label} />
       </span>
@@ -22,13 +22,13 @@ function Row({ desc, value, label }: RowProps) {
 
 function Pointer({ label }: { label: string }) {
   return (
-    <div className="absolute w-fit flex items-center gap-2 left-full top-1/2 -translate-y-1/2">
-      <div className="h-px bg-[#BFBFC4] w-[6.5rem] relative">
-        <div className="size-1 bg-[#BFBFC4] rotate-45 absolute right-0 top-1/2 -translate-y-1/2" />
-      </div>
-      <div className="font-mono text-xs bg-black px-1.5 py-1 rounded-md text-white">
+    <div className="absolute flex items-center gap-2 left-full top-1/2 transform -translate-y-1/2">
+      <span className="h-px bg-gray-400 w-20 relative">
+        <span className="size-1 bg-gray-400 rotate-45 absolute right-0 top-1/2 -translate-y-1/2" />
+      </span>
+      <span className="font-mono text-xs bg-black px-1 py-0.5 rounded-md text-white">
         {label}
-      </div>
+      </span>
     </div>
   );
 }
@@ -59,15 +59,15 @@ export function UserDetails() {
   if (!user || !session) return null;
 
   return (
-    <div className="p-16 rounded-lg border border-[#EDEDED] bg-[#F1F1F2] relative">
-      <div className="p-8 rounded-xl bg-white shadow-lg ring-1 ring-gray-950/5 max-w-[25rem]">
+    <section className="p-8 border rounded-lg bg-gray-100 relative max-w-md">
+      <div className="p-6 rounded-lg bg-white shadow-md ring-1 ring-gray-200">
         <div className="flex flex-col items-center gap-2 mb-6">
-          <div className="relative flex justify-center items-center gap-5">
+          <div className="relative">
             <img src={user.imageUrl} className="size-20 rounded-full" alt="User Avatar" />
             <Pointer label="user.imageUrl" />
           </div>
           {user.firstName && user.lastName ? (
-            <h1 className="text-[1.0625rem] font-semibold relative">
+            <h1 className="text-base font-semibold relative">
               {user.firstName} {user.lastName}
               <Pointer label="user.firstName user.lastName" />
             </h1>
@@ -76,15 +76,15 @@ export function UserDetails() {
           )}
         </div>
 
-        <div className="px-2.5 bg-[#FAFAFB] rounded-lg divide-y divide-[#EEEEF0]">
+        <div className="bg-gray-50 rounded-lg divide-y divide-gray-200">
           <Row desc="Email" value={user.emailAddresses[0].emailAddress} label="user.emailAddresses[0].emailAddress" />
           <Row desc="Last signed in" value={formatDate(user.lastSignInAt!)} label="user.lastSignInAt" />
           <Row desc="Joined on" value={formatDate(user.createdAt!)} label="user.createdAt" />
           <Row desc="User ID" value={user.id} label="user.id" />
         </div>
 
-        <h2 className="mt-6 mb-4 text-[0.9375rem] font-semibold">Session details</h2>
-        <div className="px-2.5 bg-[#FAFAFB] rounded-lg divide-y divide-[#EEEEF0]">
+        <h2 className="mt-6 mb-2 text-sm font-semibold">Session details</h2>
+        <div className="bg-gray-50 rounded-lg divide-y divide-gray-200">
           <Row desc="Session ID" value={session.id} label="session.id" />
           <Row desc="Status" value={session.status} label="session.status" />
           <Row desc="Last active" value={formatDateTime(session.lastActiveAt)} label="session.lastActiveAt" />
@@ -93,8 +93,8 @@ export function UserDetails() {
 
         {organization && (
           <>
-            <h2 className="mt-6 mb-4 text-[0.9375rem] font-semibold">Organization detail</h2>
-            <div className="px-2.5 bg-[#FAFAFB] rounded-lg divide-y divide-[#EEEEF0]">
+            <h2 className="mt-6 mb-2 text-sm font-semibold">Organization details</h2>
+            <div className="bg-gray-50 rounded-lg divide-y divide-gray-200">
               <Row desc="Organization ID" value={organization.id} label="organization.id" />
               <Row desc="Name" value={organization.name} label="organization.name" />
               <Row desc="Members" value={String(organization.membersCount)} label="organization.membersCount" />
@@ -103,6 +103,6 @@ export function UserDetails() {
           </>
         )}
       </div>
-    </div>
+    </section>
   );
 }
